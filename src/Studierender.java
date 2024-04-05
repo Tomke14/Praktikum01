fdd153d86e5be1daa2c58abb87f8f79731fa844d
Tomke14 <165892139+Tomke14@users.noreply.github.com>
2024-04-05 10:57:56 +0200

    added Studierender class

---
+++ src/Studierender.java
@@ -0,0 +1,11 @@
+public class Studierender {
+    
+    private String name;
+    private int matrikelnummer;
+    private String studiengang;
+
+    public String frageStellen(Dozierender d1){
+
+        return frage;
+    }
+}
