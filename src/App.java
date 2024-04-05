3490899b70eca8bcd61f0d748da79e52097226e6
Tomke14 <165892139+Tomke14@users.noreply.github.com>
2024-04-05 11:57:40 +0200

    add frageStellen und sprechstundenanfrage

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     //bitte :)))
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        
     }
 }
--- src/Studierender.java
+++ src/Studierender.java
@@ -1,13 +1,67 @@
+
+import java.util.Scanner;  // Import the Scanner class
+
 public class Studierender {
     
     private String name;
     private int matrikelnummer;
     private String studiengang;
 
-    public String frageStellen(Dozierender d1){
+    public static void main(String[] args) throws Exception {
+        
+        Studierender Studi1 = new Studierender("Tomke", 1234, "UXD");
+        Studi1.frageStellen();
+        
+    }
 
-        //test
-        String frage = "";
+    
+    public Studierender(String name, int matrikelnummer, String studiengang) {
+        this.name = name;
+        this.matrikelnummer = matrikelnummer;
+        this.studiengang = studiengang;
+    }
+
+    public String frageStellen(){
+
+        Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.print("Stellen Sie ihre Frage: ");
+
+        String frage = myScanner.nextLine();  // Read user input
+        System.out.println("Ihre Frage lautet: " + frage);  // Output user input
+        
         return frage;
     }
+
+    public String sprechstundenanfrage(){
+
+        Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
+        String anfrage = myScanner.nextLine();  // Read user input
+        return anfrage;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMatrikelnummer() {
+        return matrikelnummer;
+    }
+
+    public void setMatrikelnummer(int matrikelnummer) {
+        this.matrikelnummer = matrikelnummer;
+    }
+
+    public String getStudiengang() {
+        return studiengang;
+    }
+
+    public void setStudiengang(String studiengang) {
+        this.studiengang = studiengang;
+    }
+    
 }
