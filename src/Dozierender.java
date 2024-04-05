9bcda3b23addc7e3531708f439a05599056823ed
ju <164335052+7676julia@users.noreply.github.com>
2024-04-05 10:58:41 +0200

    Klasse Dozierender

---
+++ src/Dozierender.java
@@ -0,0 +1,11 @@
+public class Dozierender {
+    private String name;
+    private String fakultaet;
+    private int bureonummer;
+
+    public String frageBeantworten (String frage) {
+        String antwort = "";
+        return antwort;
+    }
+
+}
