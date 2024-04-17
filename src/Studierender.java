e2f51c6888ab3c18c0773d2c0461c35484ae4182
Tomke14 <165892139+Tomke14@users.noreply.github.com>
2024-04-17 12:45:56 +0200

    change studierender

--- src/Studierender.java
+++ src/Studierender.java
@@ -10,7 +10,7 @@ public class Studierender {
     public static void main(String[] args) throws Exception {
         
         Studierender Studi1 = new Studierender("Tomke", 1234, "UXD");
-        Studi1.frageStellen();
+        Studi1.frageStellen(null);
         
     }
 
@@ -20,25 +20,36 @@ public class Studierender {
         this.matrikelnummer = matrikelnummer;
         this.studiengang = studiengang;
     }
+    public void frageStellen(Dozierender dozierender) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Stellen Sie Ihre Frage an den Adozierenden: ");
+        String frage = myScanner.nextLine();
+        dozierender.beantworten(frage);
+    }
+    // public String frageStellen(){
 
-    public String frageStellen(){
+    //     Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
+    //     System.out.print("Stellen Sie ihre Frage: ");
 
-        Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
-        System.out.print("Stellen Sie ihre Frage: ");
-
-        String frage = myScanner.nextLine();  // Read user input
-        System.out.println("Ihre Frage lautet: " + frage);  // Output user input
+    //     String frage = myScanner.nextLine();  // Read user input
+    //     System.out.println("Ihre Frage lautet: " + frage);  // Output user input
         
-        return frage;
+    //     return frage;
+    // }
+
+    public void sprechstundenanfrage(Dozierender dozierender, String termin) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Vereinbaren Sie einen Termin mit dem Dozierenden: ");
+        termin = myScanner.nextLine();
+        dozierender.terminBestaetigen(termin);
     }
+    // public String sprechstundenanfrage(){
 
-    public String sprechstundenanfrage(){
+    //     Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
+    //     String anfrage = myScanner.nextLine();  // Read user input
+    //     return anfrage;
 
-        Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
-        String anfrage = myScanner.nextLine();  // Read user input
-        return anfrage;
-
-    }
+    // }
 
     public String getName() {
         return name;
